ac9b8693d34e1c84d8e15ca5bb5ef4b3af77cb00
farfadet46 <farfadet46@gmail.com>
2023-08-23 22:45:09 +0200

    update objects positions

--- 7_SEG/Watchy_7_SEG.cpp
+++ 7_SEG/Watchy_7_SEG.cpp
@@ -2,12 +2,6 @@
 
 #define DARKMODE true
 
-const uint8_t BATTERY_SEGMENT_WIDTH = 7;
-const uint8_t BATTERY_SEGMENT_HEIGHT = 11;
-const uint8_t BATTERY_SEGMENT_SPACING = 9;
-const uint8_t WEATHER_ICON_WIDTH = 48;
-const uint8_t WEATHER_ICON_HEIGHT = 32;
-
 const unsigned char *numbers [10] = {num0, num1, num2, num3, num4, num5, num6, num7, num8, num9};
 
 void Watchy7SEG::drawWatchFace(){
@@ -18,52 +12,34 @@ void Watchy7SEG::drawWatchFace(){
     drawSteps();
     drawWeather();
     drawBattery();
-    display.drawBitmap(120, 77, WIFI_CONFIGURED ? wifi : wifioff, 26, 18, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+    display.drawBitmap(121, 143, WIFI_CONFIGURED ? wifi : wifioff, 26, 18, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     if(BLE_CONFIGURED){
-        display.drawBitmap(100, 75, bluetooth, 13, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+        display.drawBitmap(85, 170, bluetooth, 13, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     }
 }
 
 void Watchy7SEG::drawTime(){
-    display.setFont(&DSEG7_Classic_Bold_53);
     //Heures
-    display.drawBitmap(5, 5, numbers[currentTime.Hour/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
-    //5+30+2
-    display.drawBitmap(5+30+2, 5, numbers[currentTime.Hour%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
-    //5+30+2+30+2
-    display.drawBitmap(5+30+2+30+2, 5, colon, 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(17, 12, numbers[currentTime.Hour/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(51, 12, numbers[currentTime.Hour%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    //separe
+    display.drawBitmap(85, 12, colon, 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
     //Minute
-    display.drawBitmap(5+30+2+30+2+30+2, 5, numbers[currentTime.Minute/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
-    display.drawBitmap(5+30+2+30+2+30+2+30+2, 5, numbers[currentTime.Minute%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    display.drawBitmap(119, 12, numbers[currentTime.Minute/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(153, 12, numbers[currentTime.Minute%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    //ok farfadet46
 }
 
 void Watchy7SEG::drawDate(){
-    display.setFont(&Seven_Segment10pt7b);
-
-    int16_t  x1, y1;
-    uint16_t w, h;
-
-    String dayOfWeek = dayStr(currentTime.Wday);
-    display.getTextBounds(dayOfWeek, 5, 85, &x1, &y1, &w, &h);
-    if(currentTime.Wday == 4){
-        w = w - 5;
-    }
-    display.setCursor(85 - w, 85);
-    display.println(dayOfWeek);
-
-    String month = monthShortStr(currentTime.Month);
-    display.getTextBounds(month, 60, 110, &x1, &y1, &w, &h);
-    display.setCursor(85 - w, 110);
-    display.println(month);
-
-    display.setFont(&DSEG7_Classic_Bold_25);
-    display.setCursor(5, 120);
-    if(currentTime.Day < 10){
-    display.print("0");
-    }
-    display.println(currentTime.Day);
-    display.setCursor(5, 150);
-    display.println(tmYearToCalendar(currentTime.Year));// offset from 1970, since year is stored in uint8_t
+    //jour
+    display.drawBitmap(17, 76, numbers[currentTime.Day/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(51, 76, numbers[currentTime.Day%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    //separateur
+    display.drawBitmap(85, 76, separe, 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //separator
+    //mois
+    display.drawBitmap(119, 76, numbers[currentTime.Month/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(153, 76, numbers[currentTime.Month%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    //ok farfadet46
 }
 void Watchy7SEG::drawSteps(){
     // reset step counter at midnight
@@ -71,36 +47,32 @@ void Watchy7SEG::drawSteps(){
       sensor.resetStepCounter();
     }
     uint32_t stepCount = sensor.getCounter();
-    display.drawBitmap(10, 165, steps, 19, 23, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
-    display.setCursor(35, 190);
+    display.drawBitmap(109, 170, steps, 19, 23, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+    display.setCursor(132, 174);
+    display.setFont(&DSEG7_Classic_Regular_15);
     display.println(stepCount);
+    //ok farfadet46
 }
 void Watchy7SEG::drawBattery(){
-    //display.drawBitmap(154, 73, battery, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
-    //display.fillRect(159, 78, 27, BATTERY_SEGMENT_HEIGHT, DARKMODE ? GxEPD_BLACK : GxEPD_WHITE);//clear battery segments
     int8_t batteryLevel = 0;
     float VBAT = getBatteryVoltage();
     if(VBAT > 4.1){
         //batteryLevel = 3;
-        display.drawBitmap(154, 73, bat3, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+        display.drawBitmap(155, 142, bat3, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     }
     else if(VBAT > 3.95 && VBAT <= 4.1){
         //batteryLevel = 2;
-        display.drawBitmap(154, 73, bat2, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+        display.drawBitmap(155, 142, bat2, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     }
     else if(VBAT > 3.80 && VBAT <= 3.95){
         //batteryLevel = 1;
-        display.drawBitmap(154, 73, bat1, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+        display.drawBitmap(155, 142, bat1, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     }
     else if(VBAT <= 3.80){
         //batteryLevel = 0;
-        display.drawBitmap(154, 73, bat0, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+        display.drawBitmap(155, 142, bat0, 37, 21, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
     }
-/*
-    for(int8_t batterySegments = 0; batterySegments < batteryLevel; batterySegments++){
-        display.fillRect(159 + (batterySegments * BATTERY_SEGMENT_SPACING), 78, BATTERY_SEGMENT_WIDTH, BATTERY_SEGMENT_HEIGHT, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
-    }
-    */
+    //ok farfadet46
 }
 
 void Watchy7SEG::drawWeather(){
@@ -110,39 +82,8 @@ void Watchy7SEG::drawWeather(){
     int8_t temperature = currentWeather.temperature;
     int16_t weatherConditionCode = currentWeather.weatherConditionCode;
 
-    display.setFont(&DSEG7_Classic_Regular_39);
-    int16_t  x1, y1;
-    uint16_t w, h;
-    display.getTextBounds(String(temperature), 0, 0, &x1, &y1, &w, &h);
-    if(159 - w - x1 > 87){
-        display.setCursor(159 - w - x1, 150);
-    }else{
-        display.setFont(&DSEG7_Classic_Bold_25);
-        display.getTextBounds(String(temperature), 0, 0, &x1, &y1, &w, &h);
-        display.setCursor(159 - w - x1, 136);
-    }
-    display.println(temperature);
-    display.drawBitmap(165, 110, currentWeather.isMetric ? celsius : fahrenheit, 26, 20, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
-    const unsigned char* weatherIcon;
-
-    //https://openweathermap.org/weather-conditions
-    if(weatherConditionCode > 801){//Cloudy
-    weatherIcon = cloudy;
-    }else if(weatherConditionCode == 801){//Few Clouds
-    weatherIcon = cloudsun;
-    }else if(weatherConditionCode == 800){//Clear
-    weatherIcon = sunny;
-    }else if(weatherConditionCode >=700){//Atmosphere
-    weatherIcon = atmosphere;
-    }else if(weatherConditionCode >=600){//Snow
-    weatherIcon = snow;
-    }else if(weatherConditionCode >=500){//Rain
-    weatherIcon = rain;
-    }else if(weatherConditionCode >=300){//Drizzle
-    weatherIcon = drizzle;
-    }else if(weatherConditionCode >=200){//Thunderstorm
-    weatherIcon = thunderstorm;
-    }else
-    return;
-    display.drawBitmap(145, 158, weatherIcon, WEATHER_ICON_WIDTH, WEATHER_ICON_HEIGHT, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
+    display.drawBitmap(5, 145, numbers[temperature/10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //first digit
+    display.drawBitmap(38, 145, numbers[temperature%10], 30, 50, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK); //second digit
+    //display.println(temperature);
+    display.drawBitmap(71, 145, celsius, 26, 20, DARKMODE ? GxEPD_WHITE : GxEPD_BLACK);
 }
